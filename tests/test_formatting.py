a3e393bbb44af24cb04b94c6eebea5cc2686b434
mfievmkmkm <kiryushekuc29@gmail.com>
2026-09-03 12:26:05 +0500

    Add safe formatting and source media

---
+++ tests/test_formatting.py
@@ -0,0 +1,16 @@
+from content_os.formatting import clean_generated_post, telegram_html
+
+
+def test_sources_are_hidden_and_formatting_is_kept():
+    value="🔥 **Хук**\n\n*Мысль*\n\nИсточник: https://example.com/x"
+    assert "Источник" not in clean_generated_post(value)
+    rendered=telegram_html(value)
+    assert "<b>Хук</b>" in rendered
+    assert "<i>Мысль</i>" in rendered
+    assert "example.com" not in rendered
+
+
+def test_untrusted_html_is_escaped():
+    rendered=telegram_html("<script>x</script> <b>да</b>")
+    assert "&lt;script&gt;" in rendered
+    assert "<b>да</b>" in rendered
