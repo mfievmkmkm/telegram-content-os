2b8885bb8a8fa0aee07df42ec65bd668b27c9557
mfievmkmkm <kiryushekuc29@gmail.com>
2026-09-03 12:42:01 +0500

    Guarantee rich posts and add MP4 rendering

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -1,4 +1,4 @@
-from content_os.formatting import clean_generated_post, telegram_html
+from content_os.formatting import clean_generated_post, decorate_post, telegram_html
 
 
 def test_sources_are_hidden_and_formatting_is_kept():
@@ -14,3 +14,11 @@ def test_untrusted_html_is_escaped():
     rendered=telegram_html("<script>x</script> <b>да</b>")
     assert "&lt;script&gt;" in rendered
     assert "<b>да</b>" in rendered
+
+
+def test_decorate_guarantees_emphasis_and_emojis():
+    value="Первый хук\n\nОсновная мысль\n\nПрактический вывод\n\nЧто выберешь?"
+    decorated=decorate_post(value,"liga")
+    assert decorated.count("<b>")==1
+    assert decorated.count("<i>")==1
+    assert "⚡" in decorated and "🧠" in decorated and "⚽" in decorated
