c4bea752abfa271ca56c6a9396b4eeac665922df
mfievmkmkm <kiryushekuc29@gmail.com>
2026-09-05 16:56:28 +0500

    chore: expose growth attribution and insight primitives

--- content_os/growth/__init__.py
+++ content_os/growth/__init__.py
@@ -2,7 +2,9 @@
 
 from .analytics_v2 import GrowthSnapshot, GrowthSummary, build_growth_summary
 from .attribution import CampaignRef, FunnelSummary, build_funnel_summary
+from .cta import TrackedCTA, telegram_deep_link
 from .experiments import Experiment, ExperimentVariant, validate_experiment
+from .insights import GrowthInsight, best_observed, rank_dimension
 
 __all__ = [
     "GrowthSnapshot",
@@ -11,7 +13,12 @@ __all__ = [
     "CampaignRef",
     "FunnelSummary",
     "build_funnel_summary",
+    "TrackedCTA",
+    "telegram_deep_link",
     "Experiment",
     "ExperimentVariant",
     "validate_experiment",
+    "GrowthInsight",
+    "best_observed",
+    "rank_dimension",
 ]
