0af5be34bd20714cfaafe5d4de94e64019879df5
mfievmkmkm <kiryushekuc29@gmail.com>
2026-09-05 15:40:18 +0500

    feat: expose Shorts Studio orchestrator

--- content_os/shorts/__init__.py
+++ content_os/shorts/__init__.py
@@ -1,7 +1,9 @@
 from .models import ShortBrief, ShortScene, ShortStage
 from .presets import DELIVERY_PRESETS, VOICE_PRESETS, delivery, voice
+from .render_client import ShortRenderClient
 from .script import ShortScriptService
 from .session import ShortSessionStore
+from .orchestrator import ShortsStudio
 from .ui import brief_text, review_keyboard, style_keyboard, voice_keyboard
 
 __all__ = [
@@ -10,6 +12,8 @@ __all__ = [
     "ShortStage",
     "ShortScriptService",
     "ShortSessionStore",
+    "ShortRenderClient",
+    "ShortsStudio",
     "DELIVERY_PRESETS",
     "VOICE_PRESETS",
     "delivery",
